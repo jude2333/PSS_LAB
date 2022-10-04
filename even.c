cf94d1fec3025be9d40c6a905b74ddda18543de5
jude2333 <107063820+jude2333@users.noreply.github.com>
2022-10-04 10:05:14 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main() {
+int i;
+   printf("\nC program to print Even numbers : ");
+   for ( i = 1 ; i < 20 ; i++ )
+   {
+      i = i+1;
+      printf("%d  ", i);
+
+
+   }
+}
